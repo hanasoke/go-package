254af1c0a7566f53f8d01f45b846f90b5fc2b0af
hanasbp <hanasoke@gmail.com>
2025-12-07 13:43:03 +0700

    added logging golang

--- go.mod
+++ go.mod
@@ -2,4 +2,8 @@ module go-package
 
 go 1.24.9
 
-require github.com/gorilla/mux v1.8.1 // indirect
+require (
+	github.com/gorilla/mux v1.8.1 // indirect
+	github.com/sirupsen/logrus v1.9.3 // indirect
+	golang.org/x/sys v0.0.0-20220715151400-c0bba94af5f8 // indirect
+)
--- main.go
+++ main.go
@@ -2,8 +2,9 @@ package main
 
 import (
 	"encoding/json"
-	"log"
 	"net/http"
+
+	log "github.com/sirupsen/logrus"
 
 	"github.com/gorilla/mux"
 )
@@ -11,9 +12,21 @@ import (
 func main() {
 	r := mux.NewRouter()
 	r.HandleFunc("/hello", func(w http.ResponseWriter, r *http.Request) {
+
 		w.Header().Add("Content-Type", "application/json")
-		json.NewEncoder(w).Encode(map[string]bool{"ok": true})
+
+		// Cara 1
+		// json.NewEncoder(w).Encode(map[string]bool{"ok": true})
+
+		// Cara 2
+		res, _ := json.Marshal(map[string]bool{"ok": true})
+		w.Write(res)
 	}).Methods("GET")
+
+	log.WithFields(log.Fields{
+		"animal": "walrus",
+		"size":   10,
+	}).Info("A group of walrus emerges from the ocean")
 
 	log.Println("Server running on port 8080")
 	http.ListenAndServe(":8080", r)
