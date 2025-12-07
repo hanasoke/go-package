22babd7941bc84b6342dd353d6efaae64bc78443
hanasbp <hanasoke@gmail.com>
2025-12-07 13:18:02 +0700

    updated main.go

--- main.go
+++ main.go
@@ -1,5 +1,20 @@
 package main
 
-func main() {
+import (
+	"encoding/json"
+	"log"
+	"net/http"
 
+	"github.com/gorilla/mux"
+)
+
+func main() {
+	r := mux.NewRouter()
+	r.HandleFunc("/hello", func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Add("Content-Type", "application/json")
+		json.NewEncoder(w).Encode(map[string]bool{"ok": true})
+	}).Methods("GET")
+
+	log.Println("Server running on port 8080")
+	http.ListenAndServe(":8080", r)
 }
