bee1e1d846e1ee941eea84a6fa5ba82570a822ee
hanasbp <hanasoke@gmail.com>
2025-12-09 11:59:29 +0700

    updated main.go

--- main.go
+++ main.go
@@ -15,21 +15,21 @@ func main() {
 	config.LoadConfig()
 	config.ConnectDB()
 
+	// Verifikasi koneksi database
+	db := config.DB
+	if db == nil {
+		log.Fatal("Database connection is nil")
+	}
+
 	r := mux.NewRouter()
 	r.HandleFunc("/hello", func(w http.ResponseWriter, r *http.Request) {
-
 		w.Header().Add("Content-Type", "application/json")
-
-		// Cara 1
-		// json.NewEncoder(w).Encode(map[string]bool{"ok": true})
-
-		// Cara 2
 		res, _ := json.Marshal(map[string]bool{"ok": true})
 		w.Write(res)
 	}).Methods("GET")
 
 	r.Use(middleware.LoggingMiddleware)
 
-	log.Println("Server running on port 8080")
-	http.ListenAndServe(":8080", r)
+	log.Println("Server running on port", config.ENV.PORT)
+	http.ListenAndServe(":"+config.ENV.PORT, r)
 }
