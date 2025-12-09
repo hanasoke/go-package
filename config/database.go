1b80936178dd5f9bec0beaccb7d00ffd7bf80eda
hanasbp <hanasoke@gmail.com>
2025-12-09 08:40:51 +0700

    updated database.go

--- config/database.go
+++ config/database.go
@@ -2,6 +2,7 @@ package config
 
 import (
 	"fmt"
+	"go-package/model"
 
 	log "github.com/sirupsen/logrus"
 
@@ -12,13 +13,29 @@ import (
 var DB *gorm.DB
 
 func ConnectDB() {
-	connectionString := fmt.Sprintf("%v:%v@/%v?parseTime=true", ENV.DB_USERNAME, ENV.DB_PASSWORD, ENV.DB_DATABASE)
+	connectionString := fmt.Sprintf("%v:%v@tcp(127.0.0.1:3306)/%v?parseTime=true",
+		ENV.DB_USERNAME, ENV.DB_PASSWORD, ENV.DB_DATABASE)
+
 	db, err := gorm.Open(mysql.Open(connectionString), &gorm.Config{})
 	if err != nil {
-		panic("failed to connect database")
+		panic("failed to connect database" + err.Error())
 	}
 
 	DB = db
 
 	log.Println("Database connected")
+
+	// Lakukan
+	autoMigrate(db)
+}
+
+func autoMigrate(db *gorm.DB) {
+	err := db.AutoMigrate(
+		&model.User{},
+		// tambahkan model lainnya disini
+	)
+	if err != nil {
+		panic("failed to migrate database: " + err.Error())
+	}
+	log.Println("Database migration completed")
 }
